520a27c1794804c473946aae24bf0724e3fe993a
kyle-luo <tsjlky@gmail.com>
2019-10-05 21:18:23 -0400

    update

--- routes.js
+++ routes.js
@@ -7,13 +7,14 @@ const ejs = require('ejs');
 
 var index = fs.readFileSync(__dirname + '/pages/index.ejs', 'utf-8');
 var questions = fs.readFileSync(__dirname + '/pages/questions.ejs', 'utf-8');
-var top = fs.readFileSync(__dirname + '/pages/top.ejs', 'utf-8');
+var cquestions = fs.readFileSync(__dirname + '/pages/common_questions.ejs', 'utf-8');
+// var top = fs.readFileSync(__dirname + '/pages/top.ejs', 'utf-8');
 
 
 // var partials = {head, nav, sidebar, breadcrumbs, logout, footer, bodyjs, sessionS, sessionF};
 
 routes.get('/', function(req, res) {
-	var data = ejs.render(index, {questions, top});
+	var data = ejs.render(index, {cquestions, questions});
 	res.send(data);
 });
 
