46a0396d30363e1ab4c84a8076f083a16f3bfee1
kyle-luo <tsjlky@gmail.com>
2019-12-01 00:04:34 -0500

    final

--- mysql.js
+++ mysql.js
@@ -25,7 +25,7 @@ async function addRecord(pnum, question, age, gender, ethn, q1, q2, q3, q4, q5, 
 			})
 		}
 		else {
-			let sql = "UPDATE survey SET ? WHERE pnum = " + pnum;
+			let sql = "UPDATE survey SET ? WHERE pnum = " + "'" + pnum + "'";
 			let values = {question, age, gender, ethn, q1, q2, q3, q4, q5, q6, q7, q8, q9};
 			pool.query(sql, [values], function (err, result) {
 			    if (err) return reject(err);
